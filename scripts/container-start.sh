99bb357f79d65160677360c4e2aad594d75d1462
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-11-08 02:33:08 +0530

    added new files

---
+++ scripts/container-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d --name spring-petclinic -p 8080:8080 cloudsihmar/spring-petclinic:latest
---
+++ scripts/container-stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Stop and remove the existing Docker container
+docker stop spring-petclinic
+docker rm spring-petclinic
