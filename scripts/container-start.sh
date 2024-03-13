d3821a1630043e3da8c88033772dabb55852ff21
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2024-03-13 19:14:26 +0530

    Update container-start.sh

--- scripts/container-start.sh
+++ scripts/container-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sleep 5
-source codedeploy_vars.env
+REPOSITORY_URI="685421549691.dkr.ecr.us-east-1.amazonaws.com/sandeep-repo"
+IMAGE_TAG="latest"
 docker run -d --name spring-petclinic -p 9090:8080 $REPOSITORY_URI:$IMAGE_TAG
