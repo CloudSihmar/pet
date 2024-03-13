c0d4eba2ca0b3c65d1cd45028967b459d323641d
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2024-03-13 17:57:08 +0530

    Update container-start.sh

--- scripts/container-start.sh
+++ scripts/container-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sleep 5
-docker run -d --name spring-petclinic -p 8080:8080 cloudsihmar/spring-petclinic:latest
+source codedeploy_vars.env
+docker run -d --name spring-petclinic -p 9090:8080 $REPOSITORY_URI:$IMAGE_TAG
