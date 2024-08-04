5a5e17c6d64faae2045a745807f3e7d764a1605d
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2024-08-05 01:06:40 +0530

    Create container-stop.sh

---
+++ scripts-ec2/container-stop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the container name
+container_name="spring-petclinic"
+
+# Check if the container exists
+if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
+    # Container exists, so stop and remove it
+    docker stop $container_name
+    docker rm -f $container_name
+    sleep 30
+else
+    echo "Container $container_name not found. Skipping removal."
+    
+fi
